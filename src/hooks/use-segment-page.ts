0df589cd81649cf0336c3a073258997c3f8313c0
Charles Stover <github@charlesstover.com>
2022-05-05 13:12:49 -0700

    Update use-segment-page.ts

--- src/hooks/use-segment-page.ts
+++ src/hooks/use-segment-page.ts
@@ -3,7 +3,7 @@ import MISSING_SEGMENT_CONTEXT_ERROR from '../constants/missing-segment-context-
 import PageContext from '../contexts/page';
 import type SegmentPage from '../types/segment-page';
 
-export default function useSegmentTrack(): SegmentPage {
+export default function useSegmentPage(): SegmentPage {
   const page: SegmentPage | null = useContext(PageContext);
 
   if (page === null) {
